647304f03003f6f8999beff4d8b89d6c88ac53d5
spaceforestchu <spaceforestchu@gmail.com>
2021-10-20 15:57:37 -0400

    add delete button

--- src/App.js
+++ src/App.js
@@ -19,33 +19,63 @@ export class App extends Component {
       },
     ],
     post: "",
+    isError: false,
+    errorMessage: "",
   };
 
   handleShowPostArray = () => (
-    <ul>
+    <ul style={{ listStyle: "none" }}>
       {this.state.postArray.map(({ body, id }) => (
-        <li key={id}>{body}</li>
+        <li key={id}>
+          {body} <button>Delete</button>
+        </li>
       ))}
     </ul>
   );
 
   handleInputChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+    // if (event.target.value.length > 0) {
+    //   this.setState({
+    //     isError: false,
+    //     errorMessage: "",
+    //   });
+    // }
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => {
+        //this.setState has a callback function that allows us to call
+        //after this.setState is finished executing
+        if (this.state.post.length > 0) {
+          this.setState({
+            isError: false,
+            errorMessage: "",
+          });
+        }
+      }
+    );
   };
 
   handlePostSubmit = (event) => {
     event.preventDefault();
 
-    let newArray = [
-      ...this.state.postArray,
-      { id: uuidv4(), body: this.state.post },
-    ];
+    if (this.state.post.length === 0) {
+      this.setState({
+        isError: true,
+        errorMessage: "Sorry, you cannot submit an empty field",
+      });
+    } else {
+      let newArray = [
+        ...this.state.postArray,
+        { id: uuidv4(), body: this.state.post },
+      ];
 
-    this.setState({
-      postArray: newArray,
-    });
+      this.setState({
+        postArray: newArray,
+        post: "",
+      });
+    }
   };
 
   /*
@@ -53,10 +83,13 @@ export class App extends Component {
     2. if the input is empty and a user tried to submit, it should show up an error message "Cannot submit an empty field"
     3. as soon as there value in the input field the error message is cleared
     4. using css make the bullet points go away
+
+    5. if value already exists please show an error message telling user "No duplicate!"
+    6. Delete button
   */
 
   render() {
-    const { post } = this.state;
+    const { post, isError, errorMessage } = this.state;
 
     return (
       <div className="App">
@@ -64,7 +97,7 @@ export class App extends Component {
           <input name="post" value={post} onChange={this.handleInputChange} />
           <button>Submit</button>
         </form>
-
+        {isError ? <span style={{ color: "red" }}>{errorMessage}</span> : ""}
         {this.handleShowPostArray()}
       </div>
     );
